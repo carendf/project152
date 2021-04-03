29260a31e10473412818b8aba3a2f554f0d4b5c7
Rifera839 <72307038+Rifera839@users.noreply.github.com>
2021-04-03 07:38:55 +0700

    Menguploud Tugas Portofolio Template Bootstrap

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,359 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>LARIN DILLA FHIONA - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="/profilsaya/img/favicon.png" rel="icon">
+  <link href="/profilsaya/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,500,500i,600,600i,700,700i&subset=cyrillic" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="/profilsaya/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/profilsaya/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="/profilsaya/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="/profilsaya/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="/profilsayaassets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="/profilsaya/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Lonely - v4.1.0
+  * Template URL: https://bootstrapmade.com/free-html-bootstrap-template-lonely/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column align-items-center justify-content-center">
+    <h1>Hi, I'm LARIN DILLA FHIONA!</h1>
+    <a href="#about" class="btn-get-started scrollto"><i class="bi bi-chevron-double-down"></i></a>
+  </section><!-- End Hero -->
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <h1><a href="index.html">LOVELY</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">Tentang Saya </a></li>
+          <li><a class="nav-link scrollto" href="#resume">Riwayat Pendidikan</a></li>
+          <li><a class="nav-link scrollto" href="#contact"> No.Telpon dll</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row no-gutters">
+          <div class="image col-xl-5 d-flex align-items-stretch justify-content-center justify-content-lg-start"></div>
+          <div class="col-xl-7 ps-0 ps-lg-5 pe-lg-1 d-flex align-items-stretch">
+            <div class="content d-flex flex-column justify-content-center">
+              <h3>
+              </h3>
+              <p>
+              </p>
+              <div class="row">
+                <div class="col-md-10 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                   
+                   
+                    <p><strong> NAMA  : LARIN DILLA FHIONA </strong>  </p>
+                  </div>
+                </div>
+
+                <div class="col-md-10 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                   
+                   
+                    <p><strong> NIM : 1810530152 </strong> </p>
+                  </div>
+                </div>
+
+                <div class="col-md-10 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                
+                   
+                    <p><strong> KELAS : 6C </strong> </p>
+                  </div>
+                </div>
+
+                <div class="col-md-10 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                   
+                  
+                    <p><strong> JURUSAN : ILKOM SI</strong> </p>
+                  </div>
+                </div>
+              </div>
+            </div><!-- End .content-->
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Skills Section ======= -->
+    <section id="skills" class="skills section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Skills</h2>
+          <p>
+          </p>
+        </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-6">
+
+            <div class="progress">
+              <span class="skill">HTML <i class="val">40%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">CSS <i class="val">40%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">JavaScript <i class="val">40%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6">
+
+            <div class="progress">
+              <span class="skill">PHP <i class="val">40%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">WordPress/CMS <i class="val">30%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Photoshop <i class="val">60%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Skills Section -->
+
+    <!-- ======= Riwayat pendidikan ======= -->
+    <!-- ======= Resume Section ======= -->
+    <section id="resume" class="resume section-bg">
+      <div class="container">
+
+        <div class="section-title">
+        <h2>Riwayat pendidikan</h2>
+          <p> Baik disini saya akan menjelaskan riwayat hidup saya.
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+
+
+            <h3 class="resume-title">Pendidikan</h3>
+            <div class="resume-item">
+              <h4>TK</h4>
+              <h5>SEKAR PERTIWI BANDUNG</h5>
+              <p><em>disini saya mendapatkan teman baik,sahabat,musuh,dll
+                     guru yang selalu memotivasi anak-anak didiknya,
+                     dan terakhir ada acara berkebun,berkunjung ke kebun binatang,memetik dll,
+                     acara terakhir di tk tersebut ada pementasan tari untuk pisah kenang.2005-2006</em></p>
+            
+            </div>
+            <div class="resume-item">
+              <h4>SDN 2 RAKAM Selong</h4>
+              <h4>SDN 3 Masbagik Selatan</h4>
+           
+              <p>banyak teman baik,
+              masih hobi makan nasi goreng
+              beberok pindang</em></p>
+           
+            </div>
+            <div class="resume-item">
+              <h4>SMPN 5 MASBAGIK SELATAN</h4>
+             
+           
+              <p>MENGIKUTI 3 KEGIATAN OSIS,PMR,PILDACIL
+              SETIAP MENDEKATI PUASA</em></p>
+           
+            </div>
+            <div class="resume-item">
+              <h4>SMKN 1 SELONG</h4>
+             
+           
+              <p>Disini saya mengikuti 2 kegiatan 
+              yaitu teater dan osis</em></p>
+           
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <h3 class="resume-title">Pengalaman</h3>
+            <div class="resume-item">
+              <h4>Dealer honda aikmel</h4>
+           
+              <p><em>kerjaan adalah pengalaman hidup.
+              yang harus ditekuni,ulet,
+              tanggung jawab dll.</em></p>
+             
+            </div>
+            <div class="resume-item">
+              <h4>Kuliah di UNIVERSITAS BUMIGORA MATARAM</h4>
+            
+              <p><em>MENGIKUTI KEAKTIFAN 
+              ESKUL KAMPUS
+              YAITU KAMIL,ESC</em></p>
+             
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- End Resume Section -->
+
+        <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2> No.Telpon dll</h2>
+          <p>
+          </p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-4 col-md-4">
+            <div class="contact-about">
+              <h3>LoVELY</h3>
+              <p>
+              </p>
+              <div class="social-links">
+                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="info">
+              <div class="d-flex align-items-center">
+                <i class="bi bi-geo-alt"></i>
+                <p>Lombok Timur<br>Masbagik, NY 082340091365</p>
+              </div>
+
+              <div class="d-flex align-items-center mt-4">
+                <i class="bi bi-envelope"></i>
+                <p>larindillafhiona99@gmail.com</p>
+              </div>
+
+              <div class="d-flex align-items-center mt-4">
+                <i class="bi bi-phone"></i>
+                <p>+62 082340091365</p>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-lg-5 col-md-8">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="form-group">
+                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+              </div>
+              <div class="form-group mt-3">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+ 
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="/profilsaya/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/profilsaya/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="/profilsaya/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="/profilsaya/vendor/php-email-form/validate.js"></script>
+  <script src="/profilsaya/vendor/purecounter/purecounter.js"></script>
+  <script src="/profilsaya/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="/profilsaya/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="/profilsaya/js/main.js"></script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/larinprofil', function () {
+    return view('profil');
+});
